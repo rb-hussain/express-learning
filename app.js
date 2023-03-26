7bb45c4965f1b778525c073c39ecb80d0b88580f
Rabi <rbhussain990@gmail.com>
2023-03-27 01:24:52 +0500

    Setup Post method and install body parser pkg to get form user form data through post method

--- app.js
+++ app.js
@@ -2,14 +2,48 @@
 const express = require ("express");
 const app = express();
 const port = 4000;
+// Import Path
+const path = require("path");
+// Import Body Parser 
+const bodyParser = require("body-parser");
 // Set Response on  / Request 
 
-app.get("/", (req,res)=>{
+// Get Method
 
-    res.send("<h1>Hello World.!</h1>")
+// app.get("/", (req,res)=>{
+//     res.send("<h1>Hello World.!</h1>")
+// })
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname+"/form.html"));
+});
+
+app.post("/api/v1/login",(req,res)=>{
+    res.send("Done");
 })
+
+// Post Method
+// app.post()
+
 
 // Creating a server on port 4000;
 app.listen(port, ()=>{
     console.log(`Server is working on port ${port}`);
 });
+
+
+
+
+// Normal FullStack have these funcanality
+// C       . R   . U          . D
+//create, read , update ,   delete
+
+
+// In Express We have these four methods
+// Get,  Post ,  Put,       Delete
+// Read, Create, update,    delete
+
+
+
+
+
