95d7c3b0dcc841fe27145e6ea05dfc9b69757821
Rabi <rbhussain990@gmail.com>
2023-03-27 14:35:22 +0500

    Creating Get and Post API

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended:false}));
-
+app.use(express.json());
 // Set Response on  / Request 
 
 // Get Method
@@ -21,9 +21,33 @@ app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname+"/form.html"));
 });
 
+app.get("/api/v1/userdata", (req,res) =>{
+
+    res.json({
+        name:"Rabi",
+        email:"sample@gmail.com",
+        password:"Helllo",
+    });
+});
+
+app.post("/api/v1/registeruser",(req,res)=>{
+
+    const userName = req.body.name;
+    const userEmail = req.body.email;
+    const userPassword = req.body.password;
+
+    res.json({
+        sucess:true,
+        // name: userName,
+        // email:userEmail,
+        // password:userPassword,
+    });
+});
+
+
 app.post("/api/v1/login",(req,res)=>{
     console.log(req.body)
-    res.send(`Welcome MR.  ${req.body.name}`);
+    res.send(`Welcome MR. ${req.body.name}`);
 })
 
 // Post Method
